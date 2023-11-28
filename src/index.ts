042d8d6421495246b9eb9309e1ff65eabe0f55f5
Юлия <87200821+Juicyyyy@users.noreply.github.com>
2023-11-28 12:53:31 +0400

    task2

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import "./icons";
 import Router from "./router";
 import { cartService } from "./services/cart.service";
 import { userService } from "./services/user.service";
-import { favoritesComp } from "./modules/favorites/favorites"
 
 new Router();
 cartService.init();
 userService.init();
-favoritesComp.init();
 
 setTimeout(() => {
   document.body.classList.add("is__ready");
